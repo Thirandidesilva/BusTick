cea60c830a838983c4096e120e44216fb5865547
Thirandidesilva <Thirandidesilva2002@gmail.com>
2024-11-28 21:27:59 +0530

    Updated main activity navigate to login page

--- app/src/main/java/com/example/proj/MainActivity.java
+++ app/src/main/java/com/example/proj/MainActivity.java
@@ -32,5 +32,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button button1 = findViewById(R.id.loginbtn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
