ba18b88f98927a289c148bbcf8730458ca44643f
Thirandidesilva <Thirandidesilva2002@gmail.com>
2024-11-28 22:30:40 +0530

    updated login and passenger registration

--- app/src/main/java/com/example/proj/LoginActivity.java
+++ app/src/main/java/com/example/proj/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.proj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
