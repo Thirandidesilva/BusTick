516425b439bef477c077667a0711cbe013dc1279
shanukaumesh <shanukaumesh0@gmail.com>
2024-12-04 15:41:13 +0530

    add DriverHomeActivity.java 2

--- app/src/main/java/com/example/proj/DriverHomeActivity.java
+++ app/src/main/java/com/example/proj/DriverHomeActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
